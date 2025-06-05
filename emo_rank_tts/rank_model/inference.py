e2e15dffeb881568e82fcd4bb945906bbddf451a
moon <0917jong@gmail.com>
2025-06-05 13:12:08 +0000

    rank model

--- emo_rank_tts/rank_model/inference.py
+++ emo_rank_tts/rank_model/inference.py
@@ -1,8 +1,11 @@
+import json
 import yaml
 import torch
 
-from model import RankModel
-from dataset import FineGrainedDataset
+import numpy as np
+from tqdm import tqdm
+from model import FineGraindModel
+from dataset import FineGrainedDataset, collate_fn
 
 def inference(config):
     
@@ -24,17 +27,30 @@ def bucketize(config):
     bucket_size         = config['inference']['bucket_size']
     speakers            = config['preprocessing']['speakers']
     emotions            = config['preprocessing']['emotions']
-    preprocessed_path   = config['paths']['preprocessed_path']
+    preprocessed_path   = config['path']['preprocessed_path']
+
+    n_mels              = config['audio']['n_mels']
+    n_heads             = config['model']['n_heads']
+    n_speakers          = len(speakers)
+    n_emotions          = len(emotions)
+    n_encoder_layers    = config['model']['n_encoder_layers']
+    hidden_dim          = config['model']['hidden_dim']
+    kernel_size         = config['model']['kernel_size']
+    dropout             = config['model']['dropout']
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    model = torch.load(model_path, map_location=device)
+    model = FineGraindModel(n_mels, n_heads, n_speakers, n_emotions, n_encoder_layers,
+                            hidden_dim, kernel_size, dropout)
+    model.load_state_dict(torch.load(model_path, map_location=device))
+    model = model.to(device)
+
 
     dataset = FineGrainedDataset(preprocessed_path, speakers, emotions, split='train')
     dataloader = torch.utils.data.DataLoader(
         dataset,
-        batch_size=1,
+        batch_size=8,
         shuffle=False,
-        collate_fn=dataset.collate_fn,
+        collate_fn=collate_fn,
         num_workers=4,
     )
 
@@ -42,7 +58,7 @@ def bucketize(config):
 
     model.eval()
     with torch.no_grad():
-        for batch in dataloader:
+        for batch in tqdm(dataloader):
 
             emo_X = batch['emo_X'].to(device)
             neu_X = batch['neu_X'].to(device)
@@ -54,21 +70,23 @@ def bucketize(config):
             B = emo_X.size(0)
             lambda_i = torch.ones(B, device=device)
             lambda_j = torch.zeros(B, device=device)
-            lambdas = torch.stack([lambda_i, lambda_j], dim=1)  # (B, 2)
+            lambdas = torch.stack([lambda_i, lambda_j], dim=0)  # (2, B)
 
             # forward pass
-            predictions = model(emo_X, neu_X, speaker, length, targets, lambdas)
+            predictions = model(emo_X, neu_X, emotion, length, lambdas)
             _, _, h, _, _, _, r, _ = predictions
 
             r = r.squeeze(-1)
+            h = h.detach().cpu().numpy()
 
             for i in range(B):
                 speaker_name = speakers[speaker[i].item()]
                 emotion_name = emotions[emotion[i].item()]
-                intensity_representations[speaker_name][emotion_name].append((r[i].item(), h[i].item()))
+                intensity_representations[speaker_name][emotion_name].append((r[i].item(), h[i]))
 
 
     # speaker의 emotion별로 (r, h) 쌍을 r을 기준으로 3개의 bin으로 만들고 각 bin의 평균 h를 구함
+    result = {speaker: {emotion: [] for emotion in emotions} for speaker in speakers}
     for speaker in speakers:
         for emotion in emotions:
             values = intensity_representations[speaker][emotion]
@@ -81,16 +99,19 @@ def bucketize(config):
 
             # 3개의 bin으로 나누기
             bin_size = len(r_values) // bucket_size
-            bins = [r_values[i:i + bin_size] for i in range(0, len(r_values), bin_size)]
             h_means = [sum(h_values[i:i + bin_size]) / bin_size for i in range(0, len(h_values), bin_size)]
 
-            print(f'Speaker: {speaker}, Emotion: {emotion}, Bins: {bins}, H Means: {h_means}')
-            
+            result[speaker][emotion] = h_means[:bucket_size]
+
+
+    # npz 저장
+    np.savez('result.npz', intensity=result)
+    print("Bucketization complete. Results saved to 'result.npz'.")
 
 
 if __name__ == '__main__':
 
-    config = yaml.safe_load(open('config.yaml'))
+    config = yaml.safe_load(open('parameter.yaml'))
     # inference(config)
 
     bucketize(config)
